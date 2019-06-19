aa1d683ff2d8cbc91370e200cd389d84b0f43136
Júnior de Freitas <juniormfreitas@gmail.com>
2019-06-19 10:11:41 -0600

    Displaying content from .MD files

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,24 @@ import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import "../scss/main.scss"
+import { graphql } from "gatsby"
 
-export default () => (
+export default ({ data }) => (
   <div style={{ margin: `3rem auto` }}>
     <Header headerText="Welcome." />
     <section className="grid-container">
       <div className="grid-x">
-        <div className="cell medium-6 large-4">
+        <div className="cell medium-12">
           <h1>Hello Gatsby!</h1>
-          <p>This is my first Gatsby tutorial using JSX and that awful CSS</p>
+          <h2>We have a total of {data.allMarkdownRemark.totalCount} posts</h2>
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <div key={node.id}>
+              <h3>
+                {node.frontmatter.title} - {node.frontmatter.date}
+              </h3>
+              <p>{node.excerpt}</p>
+            </div>
+          ))}
         </div>
         <div className="col-md-6 my-auto">
           <img src="https://source.unsplash.com/random/400x200" alt="" />
@@ -20,3 +29,23 @@ export default () => (
     <Footer />
   </div>
 )
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MM, YYYY")
+          }
+          excerpt
+          timeToRead
+          html
+        }
+      }
+    }
+  }
+`
