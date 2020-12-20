37f94594374efc4e9c5153225b6fa00e603557c4
J. de Freitas <juniormfreitas@gmail.com>
2020-12-20 12:19:02 -0600

    Install and load bootstrap dependencies and bundles properly

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,4 @@
+import "./node_modules/jquery/dist/jquery.min.js"
+import "./node_modules/bootstrap/dist/js/bootstrap.bundle.js"
 import "./node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "./src/css/clean-blog.min.css"
