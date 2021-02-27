c886de9a87b01e34478945c36e57a3a118506801
J. de Freitas <juniormfreitas@gmail.com>
2021-02-27 16:54:45 -0600

    Updates

---
+++ component---src-pages-404-js-b6c0c68845724dde088b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return a}));var o=t("q1tI"),r=t.n(o),u=t("3yDT");function a(){return r.a.createElement(u.a,{headerText:"404 - Not found!"})}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b6c0c68845724dde088b.js.map
