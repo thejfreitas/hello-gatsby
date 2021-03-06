d858885c48a430bc479204146ad6c195a650dc6d
J. de Freitas <juniormfreitas@gmail.com>
2021-03-06 12:22:21 -0600

    Remove Jquery

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,3 @@
-import "./node_modules/jquery/dist/jquery.min.js"
 import "./node_modules/bootstrap/dist/js/bootstrap.bundle.js"
 import "./node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "./src/css/clean-blog.min.css"
