23426f33a21fc97bb9367351be7a130e6ff65d77
J. de Freitas <juniormfreitas@gmail.com>
2021-10-24 10:50:15 -0600

    Updates

---
+++ component---src-templates-post-js-2268a9c9ccd3a8b196dc.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[851],{9857:function(e,t,r){r.r(t),r.d(t,{default:function(){return l}});var a=r(7294),n=r(5616);function l(e){var t=e.data.markdownRemark;return a.createElement(n.Z,{headerText:t.frontmatter.title},a.createElement("h1",null,t.frontmatter.title),a.createElement("div",{dangerouslySetInnerHTML:{__html:t.html}}))}}}]);
+//# sourceMappingURL=component---src-templates-post-js-2268a9c9ccd3a8b196dc.js.map
