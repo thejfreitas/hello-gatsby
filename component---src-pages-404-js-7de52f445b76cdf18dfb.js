d17f56e2b9d8af5dfb6cf18dcd2e05762e2b580d
J. de Freitas <juniormfreitas@gmail.com>
2022-03-08 15:36:32 -0600

    Updates

---
+++ component---src-pages-404-js-7de52f445b76cdf18dfb.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[883],{429:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(7294),u=r(8398);function a(){return n.createElement(u.Z,{headerText:"404 - Not found!"})}}}]);
+//# sourceMappingURL=component---src-pages-404-js-7de52f445b76cdf18dfb.js.map
