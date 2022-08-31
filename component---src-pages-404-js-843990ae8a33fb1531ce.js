4805f93c3701621478c199de03b1339df70c5f2d
J. de Freitas <juniormfreitas@gmail.com>
2022-08-31 08:31:31 -0600

    Updates

--- component---src-pages-404-js-843990ae8a33fb1531ce.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[883],{9616:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(7294),u=r(5616);function a(){return n.createElement(u.Z,{headerText:"404 - Not found!"})}}}]);
-//# sourceMappingURL=component---src-pages-404-js-843990ae8a33fb1531ce.js.map
--- component---src-templates-post-js-a8bf1d344d0b12eb4452.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[851],{9857:function(e,t,r){"use strict";r.r(t),r.d(t,{default:function(){return l}});var a=r(7294),n=r(5616);function l(e){var t=e.data.markdownRemark;return a.createElement(n.Z,{headerText:t.frontmatter.title},a.createElement("h1",null,t.frontmatter.title),a.createElement("div",{dangerouslySetInnerHTML:{__html:t.html}}))}}}]);
-//# sourceMappingURL=component---src-templates-post-js-a8bf1d344d0b12eb4452.js.map
